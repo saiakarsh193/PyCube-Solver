6e61f6374718726ec7e2793dc5b012f22a122799
saiakarsh193 <sai.akarsh@research.iiit.ac.in>
2021-11-28 12:04:12 +0530

    Updated demo

--- demo.py
+++ demo.py
@@ -4,24 +4,13 @@ from helper import getScramble
 
 cb = Cube()
 
-# to manually scramble the cube uncomment the following
-# cb.doMoves("RUR'U'")
-
-# to auto scramble the cube
 scramble = getScramble(10)
 print("Scramble:", scramble)
 cb.doMoves(scramble)
-
 print(cb)
 
 solver = Solver(cb)
 solver.solveCube(optimize=True)
 
-# to get raw moves uncomment the following
-# moves = solver.getMoves()
-# for move in moves:
-#     print(move)
-
-# or for more detailed, decorated and condensed output
 moves = solver.getMoves(decorated=True)
 print(moves)
